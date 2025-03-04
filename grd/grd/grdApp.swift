7469bffd82af55b1e5d47f9ed04e3e364754f158
n1k1takozhukhov <noloyaltyprod@gmail.com>
2025-03-04 19:10:17 +0500

    setup project

---
+++ grd/grd/grdApp.swift
@@ -0,0 +1,17 @@
+//
+//  grdApp.swift
+//  grd
+//
+//  Created by Никита Кожухов on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct grdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
