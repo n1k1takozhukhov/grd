1bf45718e0bb4bf8f7c3232d957060549414c244
n1k1takozhukhov <noloyaltyprod@gmail.com>
2025-03-05 22:04:46 +0500

    merge in another project

---
+++ grd/Services/StocksAPI.swift
@@ -0,0 +1,10 @@
+import Foundation
+import XCAStocksAPI
+
+protocol StocksAPI {
+    func searchTickers(query: String, isEquityTypeOnly: Bool) async throws -> [Ticker]
+    func fetchQuotes(symbols: String) async throws -> [Quote]
+    func fetchChartData(tickerSymbol: String, range: ChartRange) async throws -> ChartData?
+}
+
+extension XCAStocksAPI: StocksAPI {}
--- grd/grdApp.swift
+++ grd/grdApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct grdApp: App {
+    
+    @StateObject var appVM = AppViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            NavigationStack {
+                MainListView()
+            }
+        }.environmentObject(appVM)
     }
 }
